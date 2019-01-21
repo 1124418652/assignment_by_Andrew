cfc11b33b981be14954a3db4b52451fe25612c80
xhj <1124418652@qq.com>
2019-01-21 23:11:05 +0800

    dnn.py

---
+++ build_DNN/dnn.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import h5py
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
+class DNN():
+	"""
+	Deep learning neural network
+	"""
+
+	def __init__(self, ):
+		pass
+
+	def initial_parameters_deep(self):
+		pass
+
+	def linear_forward(self):
+		pass
+
+	def activate_forward(self):
+		pass
+
+	def forward_propogation(self):
+		pass
+
+	def calculate_cost(self):
+		pass
+
+	def linear_backward(self):
+		pass
+
+	def activate_backward(self):
+		pass
+
+	def backward_propogation(self):
+		pass
+
